5840efeb033effb6f2a23e2d47f1ee96dd61a2ce
吉住 孝之 <takayuki.yoshizumi@cct-inc.co.jp>
2020-12-25 13:11:18 +0900

    アプリケーション、テストの配置

---
+++ SeleniumTest/IisProcess.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTest
+{
+  /// <summary>
+  /// Selenium WebDriver を使った画面テストに共通する機能を提供します。
+  /// </summary>
+  [TestClass]
+  public abstract class IisProcess
+  {
+    /// <summary>
+    /// IIS のポート
+    /// </summary>
+    private const int Port = 2020;
+
+    /// <summary>
+    /// ベースアドレス
+    /// </summary>
+    protected static readonly string BaseAddress = $"http://localhost:{Port}";
+
+    /// <summary>
+    /// アプリケーションの配置位置
+    /// </summary>
+    private static string appPath = @"C:\deploy";
+
+    /// <summary>
+    /// IIS Express のプロセス
+    /// </summary>
+    private static Process _iisProcess;
+
+    /// <summary>
+    /// IIS Express を開始します。
+    /// </summary>
+    private static void StartIIS()
+    {
+      if (_iisProcess == null)
+      {
+        var applicationPath = GetApplicationPath(appPath);
+        var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+        var iisPath = Path.Combine(programFiles, "IIS Express", "iisexpress.exe");
+        var startInfo = new ProcessStartInfo
+        {
+          FileName = iisPath,
+          Arguments = $"/path:\"{applicationPath}\" /port:{Port}",
+        };
+        _iisProcess = Process.Start(startInfo);
+      }
+    }
+
+    /// <summary>
+    /// ソリューションフォルダ直下にあるテスト対象アプリのフォルダパスを取得します。
+    /// </summary>
+    /// <returns>テスト対象アプリのフォルダパス</returns>
+    private static string GetApplicationPath(string applicationName)
+    {
+      var solutionFolder = Path.GetDirectoryName(
+          Path.GetDirectoryName(
+              Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)));
+      return Path.Combine(solutionFolder, applicationName);
+    }
+
+    /// <summary>
+    /// IIS Express を停止します。
+    /// </summary>
+    private static void StopIIS()
+    {
+      if (_iisProcess != null &&
+          _iisProcess.HasExited == false)
+      {
+        _iisProcess.Kill();
+      }
+    }
+
+    [AssemblyInitialize]
+    public static void AssemblyInitialize(TestContext testContext)
+    {
+      StartIIS();
+    }
+
+    [AssemblyCleanup]
+    public static void AssemblyCleanup()
+    {
+      StopIIS();
+    }
+  }
+}
---
+++ SeleniumTest/SeleniumTest.cs
@@ -0,0 +1,99 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTest
+{
+  /// <summary>
+  /// Selenium WebDriver を使った画面テストに共通する機能を提供します。
+  /// </summary>
+  [TestClass]
+  public class SeleniumTest : IisProcess
+  {
+    /// <summary>
+    /// WEB ブラウザのドライバーを取得します。
+    /// </summary>
+    private ChromeDriver _driver;
+
+    /// <summary>
+    /// ChromeのDriverの場所
+    /// </summary>
+    private string driverPath = "chromedriver_win32";
+
+    [TestInitialize]
+    public virtual void TestInitialize()
+    {
+      // ChromeOptionsオブジェクトを生成します。
+      // var options = new ChromeOptions();
+      // options.AddArgument("--headless");
+
+      // this._driver = new ChromeDriver(driverPath, options);
+
+      this._driver = new ChromeDriver(driverPath);
+    }
+
+    [TestCleanup]
+    public virtual void TestCleanup()
+    {
+      this._driver.Quit();
+    }
+
+    [TestMethod]
+    public void RegisterMaleTest()
+    {
+      this._driver.Navigate().GoToUrl(BaseAddress + "/Register");
+
+      this._driver.FindElementById("name").SendKeys("Yottwui");
+
+      Thread.Sleep(1000);
+
+      this._driver.FindElementById("male").SendKeys(Keys.Space);
+
+      Thread.Sleep(1000);
+
+      var reason = this._driver.FindElementById("reason");
+      var selectElement = new SelectElement(reason);
+      selectElement.SelectByValue("テレビで見た");
+
+      Thread.Sleep(1000);
+
+      this._driver.FindElementById("registerButton").Click();
+
+      var result = this._driver.FindElementById("result");
+      Assert.AreEqual(result.Text.Replace("\r\n", ""), $"登録者名： Yottwui\n性別： 男性\n登録する経緯： テレビで見た".Replace("\n", ""));
+
+      Thread.Sleep(1000);
+
+    }
+
+    [TestMethod]
+    public void RegisterFemaleTest()
+    {
+      this._driver.Navigate().GoToUrl(BaseAddress + "/Register");
+
+      this._driver.FindElementById("name").SendKeys("Peach");
+
+      Thread.Sleep(1000);
+
+      this._driver.FindElementById("female").SendKeys(Keys.Space);
+
+      Thread.Sleep(1000);
+
+      var reason = this._driver.FindElementById("reason");
+      var selectElement = new SelectElement(reason);
+      selectElement.SelectByValue("SNSで見た");
+
+      Thread.Sleep(1000);
+
+      this._driver.FindElementById("registerButton").Click();
+
+      var result = this._driver.FindElementById("result");
+      Assert.AreEqual(result.Text.Replace("\r\n", ""), $"登録者名： Peach\n性別： 女性\n登録する経緯： SNSで見た".Replace("\n", ""));
+
+      Thread.Sleep(1000);
+
+    }
+  }
+}
